4cd5baca6bbc11ed07f458b2f966ac5da03355d8
mayue42 <mayue@unity3D.com>
2019-12-06 13:43:14 +0800

    add test03

--- recommend_tianchi/test03.py
+++ recommend_tianchi/test03.py
@@ -1,8 +1,8 @@
 # 尝试使用模型
 import numpy as np
 import csv
+import pickle
 
-np.random.seed(1337)
 from keras.datasets import mnist
 from keras.utils import np_utils
 from keras.models import Sequential
@@ -10,8 +10,23 @@ from keras.layers import Dense, Activation
 from keras.optimizers import RMSprop
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
-
 from sklearn.metrics import accuracy_score
+
+np.random.seed(1337)
+n = 23291027
+
+
+def save(data, filename):
+    ff = open(filename, "wb")
+    pickle.dump(data, ff)
+    ff.close()
+
+
+def load(filename):
+    ff = open(filename, "rb")
+    data = pickle.load(ff)
+    ff.close()
+    return data
 
 
 class Big3:
@@ -45,37 +60,35 @@ def get_hour(x):
     return get_day(x) * 24 + int(x[11])
 
 
-file = open("fresh_comp_offline/tianchi_fresh_comp_train_user.csv")
-actions = csv.reader(file)
-header = next(actions)
-n = 23291027
+def init_buy_data():
+    file = open("fresh_comp_offline/tianchi_fresh_comp_train_user.csv")
+    actions = csv.reader(file)
+    _ = next(actions)
+    i = 0
+    buy = [set()] * 31
+    for row in actions:
+        user_id = row[0]
+        item_id = row[1]
+        user_item = user_id + "," + item_id
+        action = row[2]
+        c = row[4]
+        day = get_day(row[5])
+        hour = get_hour(row[5])
+        if action == "4":
+            buy[day].add(user_item)
+        i += 1
+        if i % 200000 == 0:
+            print("cal bought items progress: %.2f%%" % (i * 100 / n))
 
-i = 0
-buy = [set()] * 31
-for row in actions:
-    user_id = row[0]
-    item_id = row[1]
-    user_item = user_id + "," + item_id
-    action = row[2]
-    c = row[4]
-    day = get_day(row[5])
-    hour = get_hour(row[5])
-    if action == "4":
-        buy[day].add(user_item)
-    i += 1
-    if i % 200000 == 0:
-        print("cal bought items progress:%.2f%%" % (i * 100 / n))
+    file.close()
+    return buy
 
-file.close()
-clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
 
-result = []
-
-for d in range(10, 31):
+def init_train_data(d, buy):
     print("train day %d" % d)
     file = open("fresh_comp_offline/tianchi_fresh_comp_train_user.csv")
     actions = csv.reader(file)
-    header = next(actions)
+    _ = next(actions)
     users = {}
     i = 0
     for row in actions:
@@ -87,6 +100,7 @@ for d in range(10, 31):
         day = get_day(row[5])
         hour = get_hour(row[5])
         if day >= d:
+            i += 1
             continue
         if user_id not in users:
             users[user_id] = {item_id: [Big3(), Big3(), Big3(), Big3()]}
@@ -95,18 +109,19 @@ for d in range(10, 31):
         users[user_id][item_id][action - 1].push(10 / (d * 24 - hour))
         i += 1
         if i % 200000 == 0:
-            print("prepare train data %.2f%%" % (100 * i / n))
+            print("prepare training data %.2f%%" % (100 * i / n))
     x = []
     y = []
     pairs = []
+    print("process training data")
     for user_id in users:
         for item_id in users[user_id]:
-            l = users[user_id][item_id]
+            t = users[user_id][item_id]
             xx = []
-            xx.extend(l[0].get_values())
-            xx.extend(l[1].get_values())
-            xx.extend(l[2].get_values())
-            xx.extend(l[3].get_values())
+            xx.extend(t[0].get_values())
+            xx.extend(t[1].get_values())
+            xx.extend(t[2].get_values())
+            xx.extend(t[3].get_values())
             if (user_id + "," + item_id) in buy[d]:
                 yy = 1
             else:
@@ -114,18 +129,60 @@ for d in range(10, 31):
             x.append(xx)
             y.append(yy)
             pairs.append(user_id + ',' + item_id)
+
+    file.close()
+    return x, y, pairs
+
+
+def get_buy_data():
+    filename = "buy_each_day.pickle"
+    try:
+        buy = load(filename)
+        print("load data buy %d %d" % (len(buy), len(buy[0])))
+    except FileNotFoundError:
+        buy = init_buy_data()
+        save(buy, filename)
+
+    return buy
+
+
+def get_train_data(d, buy):
+    x_name = "x_%d.pickle" % d
+    y_name = "y_%d.pickle" % d
+    p_name = "p_%d.pickle" % d
+    try:
+        x = load(x_name)
+        y = load(y_name)
+        p = load(p_name)
+    except FileNotFoundError:
+        x, y, p = init_train_data(d, buy)
+        save(x, x_name)
+        save(y, y_name)
+        save(p, p_name)
+    return x, y, p
+
+
+def write_result(result):
+    print("write result")
+    outfile = open("out3.csv", "w")
+    for r in result:
+        outfile.write(r + "\n")
+    outfile.close()
+
+
+clf = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=0)
+
+buy = get_buy_data()
+for d in range(29, 31):
+    print("load date for %d" % d)
+    x, y, p = get_train_data(d, buy)
     if d < 30:
         print("train...")
         clf.fit(x, y)
     if d == 30:
         y = clf.predict(x)
+        result = []
         for i in range(len(x)):
             if y[i] > 0.5:
-                result.append(pairs[i])
-    file.close()
-
-print("write result")
-outfile = open("out3.csv", "w")
-for r in result:
-    outfile.write(r + "\n")
-outfile.close()
+                result.append(p[i])
+        write_result(result)
